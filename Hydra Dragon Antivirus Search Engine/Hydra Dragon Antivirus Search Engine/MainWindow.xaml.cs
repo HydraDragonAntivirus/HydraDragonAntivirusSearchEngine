2d044cc62736082ba86a377ad898660142941df4
HydraDragonAntivirus <130374789+Siradankullanici@users.noreply.github.com>
2025-02-16 14:43:00 +0300

    Unused imports

--- Hydra Dragon Antivirus Search Engine/Hydra Dragon Antivirus Search Engine/MainWindow.xaml.cs
+++ Hydra Dragon Antivirus Search Engine/Hydra Dragon Antivirus Search Engine/MainWindow.xaml.cs
@@ -6,16 +6,7 @@ using System.Text;
 using System.Text.Json;
 using System.Text.RegularExpressions;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using Microsoft.Win32;
-using log4net;
 using IOPath = System.IO.Path;
 namespace Hydra_Dragon_Antivirus_Search_Engine
 {
